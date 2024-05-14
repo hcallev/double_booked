27d7524ed9c7ec8b78caf86d8fd57ce904d57240
Hernando Calle <hernando.calle@assuresoft.com>
2024-05-14 07:30:44 -0400

    feat: overlapping events method

--- events.go
+++ events.go
@@ -8,3 +8,15 @@ type Event struct {
 func Overlaps(a, b Event) bool {
 	return a.Start < b.End && a.End > b.Start
 }
+
+func OverlappingEvents(events []Event) [][]Event {
+	overlaps := make([][]Event, 0)
+	for i, ce := range events {
+		for _, ne := range events[i+1:] {
+			if Overlaps(ce, ne) {
+				overlaps = append(overlaps, []Event{ce, ne})
+			}
+		}
+	}
+	return overlaps
+}
--- events_test.go
+++ events_test.go
@@ -60,3 +60,65 @@ func TestOverlaps(t *testing.T) {
 		})
 	}
 }
+
+func TestOverlappingEvents(t *testing.T) {
+	tests := []struct {
+		name     string
+		events   []Event
+		overlaps [][]Event
+	}{
+		{
+			name: "overlapping event",
+			events: []Event{
+				{Start: 1, End: 3},
+				{Start: 2, End: 4},
+				{Start: 4, End: 6},
+			},
+			overlaps: [][]Event{{{Start: 1, End: 3}, {Start: 2, End: 4}}},
+		},
+		{
+			name: "multiple overlapping events",
+			events: []Event{
+				{Start: 1, End: 3},
+				{Start: 2, End: 4},
+				{Start: 4, End: 6},
+				{Start: 5, End: 7},
+			},
+			overlaps: [][]Event{
+				{{Start: 1, End: 3}, {Start: 2, End: 4}},
+				{{Start: 4, End: 6}, {Start: 5, End: 7}},
+			},
+		},
+		{
+			name: "event overlapping multiple times",
+			events: []Event{
+				{Start: 1, End: 5},
+				{Start: 2, End: 4},
+				{Start: 4, End: 6},
+			},
+			overlaps: [][]Event{
+				{{Start: 1, End: 5}, {Start: 2, End: 4}},
+				{{Start: 1, End: 5}, {Start: 4, End: 6}},
+			},
+		},
+		{
+			name: "no overlaps",
+			events: []Event{
+				{Start: 5, End: 6},
+				{Start: 1, End: 2},
+				{Start: 2, End: 4},
+			},
+			overlaps: [][]Event{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := OverlappingEvents(tt.events)
+
+			assert.Len(t, got, len(tt.overlaps))
+			for _, want := range tt.overlaps {
+				assert.Contains(t, got, want)
+			}
+		})
+	}
+}
